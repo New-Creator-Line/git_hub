5769dea1b4670ca545d9ffcac7d40415f33476d7
ddeniska__a <alekcandrenkov.d@gmail.com>
2024-10-16 18:44:10 +0300

    console.log

--- Program.java
+++ Program.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
+
 public class Program {
 
     public static void main (String args[]){
-
+        Scanner in = new Scanner(System.in);
         System.out.println("Good job");
-
+        int age = in.nextInt();
+        in.close();
     }
 }
